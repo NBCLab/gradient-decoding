8dd0e0b351310feb2e7a4738b93852ee9e85d7b2
JulioAPeraza <jpera054@fiu.edu>
2022-08-10 12:32:47 -0400

    Visualize meta-analytic maps

--- figures/utils.py
+++ figures/utils.py
@@ -1,9 +1,12 @@
 """Miscellaneous functions used for plotting gradient data"""
+import gzip
 import os.path as op
+import pickle
 
 import nibabel as nib
 from matplotlib import pyplot as plt
 from neuromaps.datasets import fetch_fslr
+from nilearn import image, plotting
 from nilearn.plotting import plot_stat_map
 from surfplot import Plot
 from surfplot.utils import threshold
@@ -50,5 +53,29 @@ def plot_subcortical_gradient(subcort_grad_fnames, cmap="viridis", threshold_=No
             cmap=cmap,
             threshold=threshold_,
             title=title_,
+        )
+        plt.show()
+
+
+def plot_meta_maps(term_based_decoder_fn, n_init=0, n_maps=10, threshold=2):
+    term_decoder_file = gzip.open(term_based_decoder_fn, "rb")
+    term_decoder = pickle.load(term_decoder_file)
+    term_based_meta_maps = term_decoder.images_
+    term_features = [f.split("__")[-1] for f in term_decoder.features_]
+
+    term_meta_maps_imgs = term_decoder.masker.inverse_transform(
+        term_based_meta_maps[n_init : n_init + n_maps, :]
+    )
+    term_features_to_plot = term_features[n_init : n_init + n_maps]
+
+    for i_feature in range(n_maps):
+        feature_img_3d = image.index_img(term_meta_maps_imgs, i_feature)
+        plotting.plot_stat_map(
+            feature_img_3d,
+            draw_cross=False,
+            colorbar=True,
+            annotate=False,
+            threshold=threshold,
+            title=term_features_to_plot[i_feature],
         )
         plt.show()
