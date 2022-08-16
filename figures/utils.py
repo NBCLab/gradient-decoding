1709de0b8f13484471e6dd431e1de42239f3b1e8
JulioAPeraza <jpera054@fiu.edu>
2022-08-15 21:17:01 -0400

    Visualize LDA topic distributions

--- figures/utils.py
+++ figures/utils.py
@@ -1,5 +1,6 @@
 """Miscellaneous functions used for plotting gradient data"""
 import gzip
+import math
 import os.path as op
 import pickle
 
@@ -79,3 +80,29 @@ def plot_meta_maps(term_based_decoder_fn, n_init=0, n_maps=10, threshold=2):
             title=term_features_to_plot[i_feature],
         )
         plt.show()
+
+
+def plot_top_words(model, feature_names, n_top_words, title):
+    n_topics = len(model.components_)
+    n_cols = 5
+    n_rows = math.ceil(n_topics / n_cols)
+    w = 30
+    h = (w / 2) * n_rows
+    fig, axes = plt.subplots(n_rows, n_cols, figsize=(w, h), sharex=True)
+    axes = axes.flatten()
+    for topic_idx, topic in enumerate(model.components_):
+        top_features_ind = topic.argsort()[: -n_top_words - 1 : -1]
+        top_features = [feature_names[i] for i in top_features_ind]
+        weights = topic[top_features_ind]
+
+        ax = axes[topic_idx]
+        ax.barh(top_features, weights, height=0.7)
+        ax.set_title(f"Topic {topic_idx +1}", fontdict={"fontsize": 30})
+        ax.invert_yaxis()
+        ax.tick_params(axis="both", which="major", labelsize=20)
+        for i in "top right left".split():
+            ax.spines[i].set_visible(False)
+        fig.suptitle(title, fontsize=40)
+
+    plt.subplots_adjust(top=0.977, bottom=0.05, wspace=0.90, hspace=0.1)
+    plt.show()
