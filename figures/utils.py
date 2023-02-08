41699357a81ccf2433b3a86554e3f987f4338326
JulioAPeraza <jpera054@fiu.edu>
2023-02-07 19:11:27 -0500

    Add plot profile function

--- figures/utils.py
+++ figures/utils.py
@@ -6,6 +6,7 @@ import pickle
 
 import nibabel as nib
 import numpy as np
+import seaborn as sns
 from matplotlib import pyplot as plt
 from neuromaps.datasets import fetch_fslr
 from nilearn import image, masking, plotting
@@ -134,4 +135,96 @@ def plot_top_words(model, feature_names, n_top_words, title):
         fig.suptitle(title, fontsize=40)
 
     plt.subplots_adjust(top=0.977, bottom=0.05, wspace=0.90, hspace=0.1)
+    plt.show()
+
+
+def plot_profile(data_df, metric, hue_order, cmap="tab20"):
+    sns.set(style="whitegrid")
+
+    my_cmap = plt.get_cmap(cmap)
+    n_approaches = len(hue_order)
+    n_segments = 30
+    fig, axes = plt.subplots(n_segments, 2)
+    fig.set_size_inches(15, 90)
+
+    for seg_sol in range(n_segments):
+        test_df = data_df[data_df["segment_solution"] == seg_sol + 3]
+        sub_data_df = test_df.drop(columns=["segment_solution", "pvalue"])
+        sub_data_df = sub_data_df.pivot_table(
+            values=metric, index=sub_data_df["segment"], columns="method"
+        )
+        sub_data_df = sub_data_df.reindex(hue_order, axis=1)
+
+        sub_data_df.plot.bar(
+            rot=0,
+            width=1,
+            stacked=True,
+            color=my_cmap.colors[: len(hue_order)],
+            ax=axes[seg_sol, 0],
+        )
+        axes[seg_sol, 0].get_legend().remove()
+
+        if seg_sol == 0:
+            handles, labels = axes[0, 0].get_legend_handles_labels()
+
+        test_df = test_df.reset_index()
+        test_df["segment"] = test_df["segment"].astype(str)
+        """
+        sns.barplot(
+            data=test_df, 
+            x="segment", 
+            y="max_corr", 
+            palette=cmap, 
+            hue="method", 
+            hue_order=hue_order, 
+            dodge=True,
+            ax=axes[seg_sol , 1],
+        )
+        axes[seg_sol , 1].get_legend().remove()
+        """
+        # x = test_df["segment"]
+        # y = test_df["max_corr"]
+        # axes[seg_sol , 2].plot(x, y, 'o-')
+        # axes[seg_sol , 2].get_legend().remove()
+        sns.lineplot(
+            data=test_df,
+            x="segment",
+            y=metric,
+            palette=cmap,
+            hue="method",
+            hue_order=hue_order,
+            marker="o",
+            ax=axes[seg_sol, 1],
+        )
+        axes[seg_sol, 1].get_legend().remove()
+
+        text_lst = []
+        mean_lst = []
+        for approach in hue_order:
+            approach_df = test_df[test_df["method"] == approach]
+            # print(approach_df)
+            mean_corr = approach_df[metric]
+            text_lst.append(f"{mean_corr.mean():.3f} ± {mean_corr.std():.3f}")
+            mean_lst.append(mean_corr.mean())
+
+        ax_handles, ax_labels = axes[seg_sol, 1].get_legend_handles_labels()
+        sort_idx = np.argsort(-np.array(mean_lst))
+        axes[seg_sol, 1].legend(
+            np.array(ax_handles)[sort_idx],
+            np.array(text_lst)[sort_idx],
+            loc="upper left",
+            bbox_to_anchor=(1.04, 1.15),
+            ncol=1,
+        )
+
+    fig.legend(
+        handles,
+        labels,
+        loc="lower center",
+        ncol=6,
+        bbox_to_anchor=(0.5, -0.01),
+    )
+
+    fig.tight_layout()
+    # plt.savefig(op.join(result_dir, "gradient_segmentation", "Figures", "correlation_profile.png"), dpi=300, bbox_inches="tight")
     plt.show()
